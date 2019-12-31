a50d93387c5d905ba26afc4e7149c68dfe299bad
Romain Sorrenti <romainsorrnti@gmail.com>
2019-12-31 20:10:13 +0100

    Dispatching custom events

--- modal.js
+++ modal.js
@@ -18,88 +18,114 @@ class Modal extends HTMLElement {
             }
 
             :host([opened]) #backdrop,
-            :host([opened]) #modal
-             {
-              opacity: 1;
-              poiter-events: all;
+            :host([opened]) #modal {
+                opacity: 1;
+                pointer-events: all;
             }
 
             #modal {
-              position: fixed;
-              top: 15vh;
-              left: 25%;
-              width: 50%;
-              background: white;
-              z-index: 100;
-              border-radius: 3px;
-              box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-              display: flex;
-              flex-direction: column;
-              justify-content: space-between;
-              opacity: 0;
-              pointer-events: none;
+                position: fixed;
+                top: 15vh;
+                left: 25%;
+                width: 50%;
+                z-index: 100;
+                background: white;
+                border-radius: 3px;
+                box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
             }
 
             header {
-              padding: 1rem;
+                padding: 1rem;
             }
-            
+
             ::slotted(h1) {
-              font-size: 1.50rem;
+                font-size: 1.25rem;
             }
 
             #main {
-              padding: 1rem;
-
+                padding: 1rem;
             }
 
             #actions {
-              border-top: 1px solid #ccc;
-              padding: 1rem;
-              display: flex;
-              justify-content: flex-end;
+                border-top: 1px solid #ccc;
+                padding: 1rem;
+                display: flex;
+                justify-content: flex-end;
             }
 
             #actions button {
-              margin: 0 0.25rem;
+                margin: 0 0.25rem;
             }
         </style>
         <div id="backdrop"></div>
         <div id="modal">
-          <header>
-            <slot name="title">Please confirm the payment</slot>
-          </header>
-          <section id="main">
-            <slot name="main"></slot>
-          </section>
-          <section id="actions">
-            <button>Cancel</button>
-            <button>Confirm</button>
-          </section>
+            <header>
+                <slot name="title">Please Confirm Payment</slot>
+            </header>
+            <section id="main">
+                <slot></slot>
+            </section>
+            <section id="actions">
+                <button id="cancel-btn">Cancel</button>
+                <button id="confirm-btn">Okay</button>
+            </section>
         </div>
     `;
-    const slots = this.shadowRoot.querySelectorAll('slot');
-    slots[1].addEventListener('slotchange', event => {
+    const slots = this.shadowRoot.querySelectorAll("slot");
+    slots[1].addEventListener("slotchange", event => {
       console.dir(slots[1].assignedNodes());
-    } )
+    });
+    const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
+    const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+    cancelButton.addEventListener("click", this._cancel.bind(this));
+    confirmButton.addEventListener("click", this._confirm.bind(this));
+    // cancelButton.addEventListener('cancel', () => {
+    //   console.log('Cancel inside the component');
+    // });
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if(name === 'opened') {
-      if (this.hasAttribute('opened')) {
-        this.isOpen = true;
-      }
+    if (this.hasAttribute("opened")) {
+      this.isOpen = true;
+      // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+      // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+      // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+      // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
     } else {
       this.isOpen = false;
     }
   }
+
   static get observedAttributes() {
-    return ['opened'];
+    return ["opened"];
   }
 
   open() {
-    this.setAttribute('opened', '');
+    this.setAttribute("opened", "");
+    this.isOpen = true;
+  }
+
+  hide() {
+    if (this.hasAttribute("opened")) {
+      this.removeAttribute("opened");
+    }
+    this.isOpen = false;
+  }
+
+  _cancel(event) {
+    this.hide();
+    const cancelEvent = new Event('cancel');
+    event.target.dispatchEvent(cancelEvent);
     
+  }
+
+  _confirm() {
+    this.hide();
   }
 }
 
